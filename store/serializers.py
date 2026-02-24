ef7ad270105dedcb44b9dc1aa4473cfd5acf3ffc
Jawahar28 <jawaharn246@gmail.com>
2026-02-24 22:36:29 +0530

    Building RESTFUL API

--- store/serializers.py
+++ store/serializers.py
@@ -2,33 +2,67 @@ from rest_framework import serializers
 from store.models import Product, Collection
 from decimal import Decimal
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length = 255)
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places= 2)
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length = 255)
+#     unit_price = serializers.DecimalField(max_digits=6, decimal_places= 2)
+#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+#     # One way of relating serializers with primary keys
+#     '''collection = serializers.PrimaryKeyRelatedField(
+#         queryset = Collection.objects.all()
+#     )'''
+
+#     # Other Way is serialzing with string
+#     '''collection = serializers.StringRelatedField()'''
+
+#     # Another way is serializing with including a nested object
+#     '''collection = CollectionSerializer()'''
+
+#     # Another way is serializing with the hyperlink
+#     collection = serializers.HyperlinkedRelatedField(
+#         queryset = Collection.objects.all(),
+#         view_name = 'collection-detail',
+#     )
+
+
+    # def calculate_tax(self, product: Product):
+    #     return product.unit_price * Decimal(1.1)
+
+# Model Serializer
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','description','slug','inventory','unit_price','price_with_tax','collection']
+    
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-
-    # One way of relating serializers with primary keys
-    '''collection = serializers.PrimaryKeyRelatedField(
-        queryset = Collection.objects.all()
-    )'''
-
-    # Other Way is serialzing with string
-    '''collection = serializers.StringRelatedField()'''
-
-    # Another way is serializing with including a nested object
-    '''collection = CollectionSerializer()'''
-
-    # Another way is serializing with the hyperlink
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name = 'collection-detail',
-    )
-
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+    
+    #  Anther Method
+
+    # def update(self, instance, validated_data):
+    #     instance.unit_price = validated_data.get('unit_price')
+    #     instance.save()
+    #     return instance
+    
+    # def validate(self, data):
+    #     if data['password'] != data['confirm_password']:
+    #         return serializers.ValidationError('Passwords do not match!')
+    #     return data
+    
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title']
--- store/views.py
+++ store/views.py
@@ -17,7 +17,7 @@ def product_detail(request,id):
     return Response(id)'''
 
 # Creating Serializers Objects
-@api_view()
+'''@api_view()
 def product_list(request):
     queryset = Product.objects.all()
     serializer = ProductSerializer(
@@ -42,4 +42,52 @@ def product_detail(request, id):
 @api_view()
 def collection_detail(request, pk):
     return Response('Ok')
+'''
 
+@api_view(['GET','POST'])
+def product_list(request):
+    if request.method == 'GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(
+            queryset, many=True,
+            context={'request': request}
+            )
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.validated_data
+        #     return Response('Ok')
+        # else:
+        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Another Form of Validating Data
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # print(serializer.validated_data)
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+# Saving Object in the database
+@api_view(['GET','PUT','PATCH','DELETE'])
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == 'GET': 
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    # Deleting Items in the database using API
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('Ok')
